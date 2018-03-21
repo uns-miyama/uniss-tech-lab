9e8efef601bcc3edc70cdd1e8868b805e78af71e
藤木　博志 <fujikislack2@gmail.com>
2018-03-21 23:13:19 +0900

    iOSアプリ画面遷移処理を改修

--- iOS/iOS/ViewController.swift
+++ iOS/iOS/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 元の画面に戻る
+    @IBAction func goBack(_ segue:UIStoryboardSegue) {}
+    
+    
+    // カメラ画面に進む
+    @IBAction func goNext(_ sender:UIButton) {
+        let next = storyboard!.instantiateViewController(withIdentifier: "cameraview")
+        self.present(next,animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    
+    
 }
 
